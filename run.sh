27bc009744c492fdb793725cf4f3798e852aedfe
Kamal Kishor <89385593+kkamal11@users.noreply.github.com>
2023-07-27 10:39:14 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -23,15 +23,15 @@ export ENV=production
 if [ $ENV == "production" ]
 then
     export DB_USER=postgres
-    export DB_PASSWORD=kamaliitm211
+    export DB_PASSWORD=enter_your_password_here
     export DB_HOST=localhost
     export DB_PORT=5432
     export DB_NAME=flask_db
-    export SECRET_KEY=pw123pwshdshbdsuvydkj345@-1
-    export SECURITY_PASSWORD_SALT=1247091232kj345@-1
+    export SECRET_KEY=pw123pwshdshhjcvjh6567bdsuvydkj345@-1
+    export SECURITY_PASSWORD_SALT=124709gchgcjvjvhtrer51232kj345@-1
 else
-    export SECRET_KEY=pw123pwshdshbdsuvydkj345@-1
-    export SECURITY_PASSWORD_SALT=1247091232kj345@-1
+    export SECRET_KEY=pw123pwshdshhjcvjh6567bdsuvydkj345@-1@-1
+    export SECURITY_PASSWORD_SALT=124709gchgcjvjvhtrer51232kj345@-1
 fi
 echo "Required ${ENV} environment variables exported"
 
